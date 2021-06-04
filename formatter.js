5cd7fbd4c30125da3cdb7c4c8963754b9b378ee4
ngoakor12 <ngoakor12@gmail.com>
2021-06-04 23:14:25 +0200

    word transformation progress

--- formatter.js
+++ formatter.js
@@ -4,10 +4,22 @@ let formattedText = document.getElementById("formattedText");
 let text = "standUp\nstarted morse code\ncompleted Validate SA ID";
 let verbs = ["started", "worked on", "completed", "Reviewed"];
 let sentences = text.split("\n");
+let words = text.split(" ");
 
 formatText.addEventListener("click", () => {
 	sentences[0] = `**${
 		sentences[0].slice(0, 1).toUpperCase() + sentences[0].slice(1)
 	}**`;
 	formattedText.value = sentences.join("\n");
+	for (let i = 0; i < words.length; i++) {
+		// console.log(words[i]);
+		for (let j = 0; j < verbs.length; j++) {
+			if (words[i] == verbs[j]) {
+				console.log(words[i], verbs[j]);
+				words[i] = `**${verbs[j]}**`;
+			}
+		}
+	}
+	// formattedText.value = sentences.join("\n");
+	formattedText.value = words.join(" ");
 });
