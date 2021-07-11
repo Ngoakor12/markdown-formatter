dd3a4ad5471ea8cd8c2d608f8991496a181f027b
ngoakor12 <ngoakor12@gmail.com>
2021-07-11 20:47:24 +0200

    code working for one verb

--- formatter.js
+++ formatter.js
@@ -1,25 +1,16 @@
-let formatText = document.getElementById("submit");
-let input = document.getElementById("textbox");
-let formattedText = document.getElementById("formattedText");
-let text = "standUp\nstarted morse code\ncompleted Validate SA ID";
-let verbs = ["started", "worked on", "completed", "Reviewed"];
-let sentences = text.split("\n");
-let words = text.split(" ");
+let text = document.querySelector("#text");
+let submit = document.querySelector("#submit");
+let formattedText = document.querySelector("#formattedText");
 
-formatText.addEventListener("click", () => {
-	sentences[0] = `**${
-		sentences[0].slice(0, 1).toUpperCase() + sentences[0].slice(1)
-	}**`;
-	formattedText.value = sentences.join("\n");
-	for (let i = 0; i < words.length; i++) {
-		// console.log(words[i]);
-		for (let j = 0; j < verbs.length; j++) {
-			if (words[i] == verbs[j]) {
-				console.log(words[i], verbs[j]);
-				words[i] = `**${verbs[j]}**`;
-			}
-		}
-	}
-	// formattedText.value = sentences.join("\n");
-	formattedText.value = words.join(" ");
-});
+submit.addEventListener("click", transformText);
+
+let italics = ["started"];
+
+function transformText() {
+  let result = text.value.replace(
+    new RegExp(italics[0], "g"),
+    `**${italics[0]}**`
+  );
+  formattedText.innerHTML = result;
+  //   console.log(result);
+}
