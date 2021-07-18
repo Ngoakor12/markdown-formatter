05a9a3483ab6ea413ff8c87d1ca8dc2ae5ff03fa
ngoakor12 <ngoakor12@gmail.com>
2021-07-18 16:25:39 +0200

    added addWordsToItalicise, still buggy

--- formatter.js
+++ formatter.js
@@ -1,10 +1,22 @@
 let text = document.querySelector("#text");
 let submit = document.querySelector("#submit");
 let formattedText = document.querySelector("#formattedText");
+let wordsToItalicise = document.querySelector("#italics");
+let addWords = document.querySelector("#addWords");
+let italicisedArray = document.querySelector("#italicisedArray");
 
 submit.addEventListener("click", italiciseText);
+addWords.addEventListener("click", addWordsToItalicise);
 
 let italics = ["started", "completed", "worked on"];
+
+function addWordsToItalicise() {
+  wordsToItalicise = Array(wordsToItalicise.value.split(","));
+  wordsToItalicise.forEach((input) => {
+    italics.push(input);
+  });
+  italicisedArray.innerHTML = italics;
+}
 
 function italiciseText() {
   let inputText = text.value;
