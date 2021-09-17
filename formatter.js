4fb49a908a9602b6a2927babe96abc04d9a5439b
ngoakor12 <ngoakor12@gmail.com>
2021-09-17 20:00:48 +0200

    refactoring

--- formatter.js
+++ formatter.js
@@ -1,31 +1,31 @@
 let text = document.querySelector("#text");
 let submit = document.querySelector("#submit");
 let formattedText = document.querySelector("#formattedText");
-let wordsToItalicise = document.querySelector("#italics");
+let enteredWords = document.querySelector("#entered-words");
 let addWords = document.querySelector("#addWords");
-let italicisedArray = document.querySelector("#italicisedArray");
+let italiciseWords = document.querySelector("#italicised-words");
 
 submit.addEventListener("click", italiciseText);
-addWords.addEventListener("click", addWordsToItalicsArray);
+addWords.addEventListener("click", displayWords);
 
-let italics = [];
+function displayWords() {
+  const words = getEnteredWords(enteredWords.value);
+  italiciseWords.innerHTML = words;
+}
 
-function addWordsToItalicsArray() {
-  if (wordsToItalicise.value !== "" || wordsToItalicise.value !== undefined) {
-    if (wordsToItalicise.value.indexOf(",") !== -1) {
-      wordsToItalicise = wordsToItalicise.value.split(",");
-      italics = wordsToItalicise;
-    } else {
-      italics.push(wordsToItalicise.value);
-    }
-  }
-  italicisedArray.innerHTML = italics;
+function getEnteredWords(wordsArray) {
+  const words = wordsArray.split(",");
+  return words;
 }
 
 function italiciseText() {
+  const words = getEnteredWords(enteredWords.value);
   let input = text.value;
-  italics.forEach((word) => {
-    formattedText.value = input.replace(new RegExp(word, "g"), `**${word}**`);
+  words.forEach((word) => {
+    formattedText.value = input.replace(
+      new RegExp(`${word}`, "g"),
+      `**${word}**`
+    );
     // save previously replaced verbs
     input = formattedText.value;
   });
